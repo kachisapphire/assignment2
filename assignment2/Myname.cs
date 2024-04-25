d2a1a9d7d9276df83937bbfda9cbe8dea710fcc4
kachisapphire <mychaelesther99@gmail.com>
2024-04-25 18:44:58 +0100

    Add project files.

---
+++ assignment2/Myname.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    public class Myname
+    {
+        public string Firstname;
+        public string Lastname;
+
+        public Myname(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+
+        public void Myfullname()
+        {
+            Console.WriteLine("My name is " + Firstname + " " + Lastname);
+        }
+    }
+}
---
+++ assignment2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection.Emit;
+using assignment2;
+
+int i;
+
+for (i = 2; i <= 50; i += 2)
+{
+    Console.WriteLine(i);
+}
+
+Label:
+Console.WriteLine("Enter a number from 1-7");
+int day = int.Parse(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Today is Sunday");
+        break;
+    case 2:
+        Console.WriteLine("Today is Monday");
+        break;
+    case 3:
+        Console.WriteLine("Today is Tuesday");
+        break;
+    case 4:
+        Console.WriteLine("Today is Wednesday");
+        break;
+    case 5:
+        Console.WriteLine("Today is Thursday");
+        break;
+    case 6:
+        Console.WriteLine("Today is Friday");
+        break;
+    case 7:
+        Console.WriteLine("Today is Saturday");
+        break;
+    default:
+        Console.WriteLine("This is not a valid number");
+        goto Label;
+}
+
+Myname newname = new Myname("Esther", "Michael");
+newname.Myfullname();
+Console.ReadKey();
